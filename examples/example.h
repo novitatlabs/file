170590cf3762e353df525e6d741564c145a93459
yogesh <yogeshwrn@outlook.com>
2018-06-02 22:44:59 +0530

    example file created using the script.

---
+++ examples/example.h
@@ -0,0 +1,10 @@
+/**
+Copyrights (c) 2018 Novitat Engineering Solutions, Chennai.
+Author: yogeshwaran [yogesh@novitatlabs.com]
+Created on: Sat, Jun 2, 2018 10:44:06 PM
+**/
+#ifndef _EXAMPLE_
+#define _EXAMPLE_
+
+
+#endif
