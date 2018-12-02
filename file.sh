337eb32c84fc16482005708c4f24338718f9c733
Yogeshwaran <(none)>
2018-12-02 18:46:35 +0530

    copyright year is choosen from system date.

--- file.sh
+++ file.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 AUTHOUR_NAME="yogeshwaran"
-MAIL_ID="yogesh@novitatlabs.com"
+MAIL_ID="yogeshwaran@novitatlabs.com"
 FILE_NAME=${2//-/_}
+copyright_msg=$"/**\nCopyrights (c) $(date +'%Y') Novitat Engineering Solutions LLP, Chennai.\nAuthor: $AUTHOUR_NAME [$MAIL_ID]\nCreated on: $(date)\n**/\n"
 
 if [ $1 = "c" ] || [ $1 = "C" ]; then
-	copyright_msg=$"/**\nCopyrights (c) 2018 Novitat Engineering Solutions, Chennai.\nAuthor: $AUTHOUR_NAME [$MAIL_ID]\nCreated on: $(date)\n**/\n"
 	source_msg=$copyright_msg$"#include \"$2.h\"\n"
 	header_msg=$copyright_msg$"#ifndef _${FILE_NAME^^}_\n#define _${FILE_NAME^^}_\n\n\n#endif"
     echo -e $source_msg > $2.c
@@ -13,7 +13,6 @@ if [ $1 = "c" ] || [ $1 = "C" ]; then
 fi
 
 if [ $1 = "cpp" ] || [ $1 = "CPP" ] || [ $1 = "CPP" ]; then
-	copyright_msg=$"/**\nCopyrights (c) 2018 Novitat Engineering Solutions, Chennai.\nAuthor: $AUTHOUR_NAME [$MAIL_ID]\nCreated on: $(date)\n**/\n"
 	source_msg=$copyright_msg$"#include \"$FILE_NAME.h\"\n"
 	header_msg=$copyright_msg$"#ifndef _${FILE_NAME^^}_\n#define _${FILE_NAME^^}_\n\n\n#endif"
     echo -e $source_msg > $2.cpp
@@ -21,7 +20,6 @@ if [ $1 = "cpp" ] || [ $1 = "CPP" ] || [ $1 = "CPP" ]; then
 fi
 
 if [ $1 = "py" ] || [ $1 = "python" ]; then
-	copyright_msg=$"'''\nCopyrights (c) 2018 Novitat Engineering Solutions, Chennai.\nAuthor: $AUTHOUR_NAME [$MAIL_ID]\nCreated on: $(date)\n'''\n"
 	base_code=$copyright_msg$"\ndef main():\n\n\n\nif __name__ == \"__main__\":\n\tmain()"
     echo -e $base_code > $2.py
 fi
