69d8676b1c59b986abe9ce3d196d26e19ee02d72
Yogeshwaran <yogesh@novitatlabs.com>
2020-07-15 21:20:51 +0530

    generated example files

--- examples/example.c
+++
@@ -1,7 +0,0 @@
-/**
-Copyrights (c) 2018 Novitat Engineering Solutions, Chennai.
-Author: yogeshwaran [yogesh@novitatlabs.com]
-Created on: Sat, Jun 2, 2018 10:44:06 PM
-**/
-#include "example.h"
-
---
+++ examples/py_example.py
@@ -0,0 +1,11 @@
+'''
+# Copyrights (c) 2020 Novitat Engineering Solutions LLP, Chennai and
+# Invent Solutions Private Limited, Chennai.
+# All rights reserved.
+#
+# This program and the accompanying materials are made available
+# under the terms described in the LICENSE file which accompanies
+# this distribution. If the LICENSE file was not attached to this
+# distribution or for further clarifications, please contact
+# legal@inventsoln.com
+'''
